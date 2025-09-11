a783a28f87fe3e2ec0111f3f0f1b969b812c1c3a
Meastt <youremail@example.com>
2025-09-11 07:36:14 -0600

    updated screen on ipad 13 pro for menu bar visibility

--- App.tsx
+++ App.tsx
@@ -174,7 +174,7 @@ const App: React.FC = () => {
       </main>
       
       {/* Bottom Navigation - Mobile and iPad */}
-      <div className="lg:hidden">
+      <div className="xl:hidden">
         <BottomNav activeView={getActiveTab()} onNavigate={navigateTo} theme={theme} />
       </div>
       
